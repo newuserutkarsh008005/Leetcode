28749bda0e65d3b65311950543c0283fd797c2df
utkarsh08-05 <utkarshprakash081105@gmail.com>
2025-12-12 23:36:11 +0530

    Time: 2 ms (37.4%), Space: 10.6 MB (48.89%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void per(vector<int>&n,int ind,vector<vector<int>>&ans){
+            // Base Case 
+
+            if(ind==n.size()){
+                ans.push_back({n});
+                return ;
+            }
+        for(int i=ind;i<n.size();i++){
+            swap(n[ind],n[i]);
+            per(n,ind+1,ans);
+            swap(n[ind],n[i]);// Backtracking
+        }
+    }
+    vector<vector<int>> permute(vector<int>& n) {
+        vector<vector<int>>ans;
+        per(n,0,ans);
+        return ans;
+    }
+};
