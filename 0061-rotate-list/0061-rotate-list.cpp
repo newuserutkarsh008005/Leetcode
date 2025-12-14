f6b2dd137417e0906013612a066b0a47b227ecf2
utkarsh08-05 <utkarshprakash081105@gmail.com>
2025-12-14 18:31:31 +0530

    Time: 0 ms (100%), Space: 16.5 MB (29.9%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
 
-        // Edge cases (added)
         if (head == nullptr || head->next == nullptr || k == 0)
             return head;
 
-        // Step 1: find last node & length
         ListNode* temp = head;
         int count = 1;
 
@@ -15,14 +13,11 @@ public:
             count++;
         }
 
-        // Step 2: make list circular
         temp->next = head;
 
-        // Step 3: correct rotation count
         k = k % count;
         int x = count - k;
 
-        // Step 4: move pointers
         ListNode* prev = temp;
         temp = head;
 
@@ -32,7 +27,6 @@ public:
             x--;
         }
 
-        // Step 5: break the circle
         prev->next = nullptr;
         head = temp;
 
